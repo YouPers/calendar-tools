9af796bf056fc2e563f9f48bd28fa980a9e6d8eb
RBLU <reto.blunschi@creagy.ch>
2013-10-23 12:49:47 +0200

    allow freqency 'once' as an alternative to undefined attribute "frequency"

--- spec/recurrence_spec.js
+++ spec/recurrence_spec.js
@@ -8,22 +8,22 @@ describe('calendar-tools Seed', function () {
 // defines an event object
         var myBirthDays = {
             "start": "2013-10-23T12:00:00.000Z",
-            "end": "2013-10-23T13:00:00.000Z",
-            "allDay": false,
-            "frequency": "week",
-            "recurrence": {
+                "end": "2013-10-23T13:00:00.000Z",
+                "allDay": false,
+                "frequency": "week",
+                "recurrence": {
                 "end-by": {
                     "type": "on",
-                    "on": "2013-12-16T13:00:00.000Z"
+                        "on": "2013-12-16T13:00:00.000Z"
                 },
                 "every": 1,
-                "exceptions": []
+                    "exceptions": []
             }
         }
 
 // creates a new seed Object passing event object and options
         var seed = Seed(myBirthDays, {
-            start: new Date(), end: new Date(2020,1,1)
+            start: new Date(), end: new Date(2020, 1, 1)
         });
 
 // generates ans retrieves all instances by period
@@ -32,6 +32,7 @@ describe('calendar-tools Seed', function () {
         for (var i = 0; i < instances.length; i++) {
             var Instance = instances[i];
             console.log(Instance.start + ': ' + Instance.getNumber() + ' years');
-        };
+        }
+        ;
     });
 });
